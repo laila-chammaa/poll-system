6ab3c1131424a1a5097f6904f2384f6a3c67498e
Laila Chammaa <laylachammaa@gmail.com>
2021-10-16 15:45:15 -0400

    fix config, fix add and delete

--- frontend/poll-system/src/components/PollForm/PollForm.js
+++ frontend/poll-system/src/components/PollForm/PollForm.js
@@ -1,8 +1,29 @@
 import './PollForm.css';
 import '../../Cards.css';
 import { Button, Card, Col, Form, Row } from 'react-bootstrap';
+import { useState } from 'react';
 
-const PollForm = () => {
+const PollForm = ({
+  currentPoll = {
+    title: '',
+    description: '',
+    choices: [
+      { text: '', description: '' },
+      { text: '', description: '' }
+    ]
+  }
+}) => {
+  const [poll, setPoll] = useState(JSON.parse(JSON.stringify(currentPoll)));
+
+  const addChoice = () => {
+    poll.choices.push({ text: '', description: '' });
+    setPoll(JSON.parse(JSON.stringify(poll)));
+  };
+  const deleteChoice = (i) => {
+    poll.choices.splice(i, 1);
+    setPoll(JSON.parse(JSON.stringify(poll)));
+  };
+
   return (
     <div className="main-div">
       <Card className="card-title-div">
@@ -13,11 +34,14 @@ const PollForm = () => {
               <Form.Label className="label-style" column="lg" lg={3}>
                 Title
               </Form.Label>
-              <Col sm={9}>
+              <Col sm={8}>
                 <Form.Control
                   type="text"
+                  value={poll.title}
+                  key={poll.title}
                   className="text-box"
                   placeholder="Title"
+                  onChange={(e) => (poll.title = e.target.value)}
                 />
               </Col>
             </Form.Group>
@@ -29,69 +53,59 @@ const PollForm = () => {
               <Form.Label className="label-style" column="lg" lg={3}>
                 Description
               </Form.Label>
-              <Col sm={9}>
+              <Col sm={8}>
                 <Form.Control
                   as="textarea"
                   className="text-box"
                   id="text-area-style"
+                  value={poll.description}
                   rows={5}
                   placeholder="Description"
                 />
               </Col>
             </Form.Group>
             <fieldset>
-              <Form.Group
-                as={Row}
-                className="group-style"
-                controlId="poll-choice1"
-              >
-                {' '}
-                {/*TO-DO: Do we need controlId??*/}
-                <Form.Label className="label-style" column="lg" lg={3}>
-                  Choice 1
-                </Form.Label>
-                <Col sm={9}>
-                  <Form.Control
-                    type="text"
-                    className="text-box"
-                    placeholder="Choice"
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group
-                as={Row}
-                className="group-style"
-                controlId="poll-choice2"
-              >
-                <Form.Label className="label-style" column="lg" lg={3}>
-                  Choice 2
-                </Form.Label>
-                <Col sm={9}>
-                  <Form.Control
-                    type="text"
-                    className="text-box"
-                    placeholder="Choice"
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group
-                as={Row}
-                className="group-style"
-                controlId="poll-choice3"
-              >
-                <Form.Label className="label-style" column="lg" lg={3}>
-                  Choice 3
-                </Form.Label>
-                <Col sm={9}>
-                  <Form.Control
-                    type="text"
-                    className="text-box"
-                    placeholder="Choice"
-                  />
-                </Col>
-              </Form.Group>
+              <Form.Label className="label-style" column="lg" lg={4}>
+                Name
+              </Form.Label>
+              <Form.Label className="label-style" column="lg" lg={8}>
+                Description
+              </Form.Label>
+              {poll.choices.map((c, i) => (
+                <Form.Group
+                  as={Row}
+                  className="group-style"
+                  controlId="poll-choice1"
+                  key={i}
+                >
+                  <Col lg={1} className="choice">
+                    <Form.Control
+                      type="text"
+                      value={c.text}
+                      className="text-box name inner"
+                      placeholder="Name"
+                    />
+                    <Form.Control
+                      type="text"
+                      value={c.description}
+                      className="text-box description inner"
+                      placeholder="Description"
+                    />
+                    <Button
+                      className="delete-btn inner"
+                      onClick={() => {
+                        deleteChoice(i);
+                      }}
+                    >
+                      -
+                    </Button>
+                  </Col>
+                </Form.Group>
+              ))}
             </fieldset>
-            <Button id="add-style">Add more +</Button>
+            <Button id="add-style" onClick={() => addChoice()}>
+              Add more +
+            </Button>
             <Button type="submit" id="create-btn">
               create
             </Button>
