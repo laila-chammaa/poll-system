650a896351fff1f19a2a39ee773276a6d8535c2b
Laila Chammaa <laylachammaa@gmail.com>
2021-11-21 20:59:32 -0500

    imports

--- polllib/PollLib/src/main/java/model/Poll.java
+++ polllib/PollLib/src/main/java/model/Poll.java
@@ -1,21 +1,6 @@
 package model;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
 import javax.persistence.*;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.StringWriter;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
