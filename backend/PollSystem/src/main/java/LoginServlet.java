0cc250d2531876fa9e6e9bd76ef8891c6582a2bf
Laila Chammaa <laylachammaa@gmail.com>
2021-12-10 22:40:36 -0500

    fix servlet

--- backend/PollSystem/src/main/java/LoginServlet.java
+++ backend/PollSystem/src/main/java/LoginServlet.java
@@ -30,23 +30,21 @@ public class LoginServlet extends HttpServlet {
         if (email != null && password == null && name == null && newPassword != null && token != null) {
             result = new Gson().toJson(userManager.setNewPassword(email, newPassword, token));
         }
-
         // if email, pass and token are not null, user already signed up  and is trying to validate
         // their token
-        if (email != null && password == null && name == null && newPassword == null && token != null) {
+        else if (email != null && password == null && name == null && newPassword == null && token != null) {
             result = new Gson().toJson(userManager.validateUser(email, token));
         }
-
         // if only email is not null, user clicked on forgot password
-        if (email != null && password == null && name == null && newPassword == null) {
+        else if (email != null && password == null && name == null && newPassword == null && token == null) {
             result = new Gson().toJson(userManager.forgotPassword(email));
         }
         // if email, password, and newPassword is not null, user wishes to change their password
-        else if (email != null && password != null && newPassword != null) {
+        else if (email != null && password != null && newPassword != null && token == null) {
             result = new Gson().toJson(userManager.changePassword(email, password, newPassword));
         }
         // if email, password, and name is not null, user wishes to sign up
-        else if (email != null && password != null && name != null) {
+        else if (email != null && password != null && name != null && token == null) {
             result = new Gson().toJson(userManager.signup(email, password, name));
         }
         // if other fields are null, except for email and pw, user wishes to log in
